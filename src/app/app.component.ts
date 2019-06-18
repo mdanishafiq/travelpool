12124a05a70407b88c62b51872a9027717e8fea4
mdanishafiq <49903666+mdanishafiq@users.noreply.github.com>
2019-06-18 11:18:36 +0200

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { popupLogin } from 'solid-auth-client/dist-lib/solid-auth-client.bundle.
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ // styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
